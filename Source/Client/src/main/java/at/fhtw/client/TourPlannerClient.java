bf22f506fd37c27dd8e652ea499f69678537c81c
beabaumgartner <if21b223@technikum-wien.at>
2023-05-22 21:18:18 +0200

    javafx ins projekt gebracht

---
+++ Source/Client/src/main/java/at/fhtw/client/TourPlannerClient.java
@@ -0,0 +1,94 @@
+package at.fhtw.client;
+
+import at.fhtw.client.businessLogic.BusinessLogic;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TourPlannerClient extends Application {
+    private static final BusinessLogic businessLogic = new BusinessLogic();
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        // fxml created with SceneBuilder
+        System.out.println("fxml start");
+        Parent root = FXMLLoader.load(TourPlannerClient.class.getResource("mainWindow.fxml"));
+        System.out.println("fxml loaded");
+
+        // bootstrap "window" named stage
+        primaryStage.setTitle("Hello World");
+        System.out.println("set title");
+
+        // set scene into stage in defined size
+        primaryStage.setScene(new Scene(root, 600, 500));
+        primaryStage.setMinWidth(400);
+        primaryStage.setMinHeight(400);
+        System.out.println("set scene");
+
+        // let's go
+        primaryStage.show();
+        System.out.println("show stage");
+    }
+
+    public static void main(String[] args) {
+        //businessLogic.createSummarizeReport("C:\\Users\\Felix\\Desktop\\report.pdf");
+
+        //client gui
+        launch(args);
+
+        /*
+        Tour createTour = new Tour("Vienna-Bratislava tour", "Vienna-Bratislava (car)", "Vienna", "Bratislava", "car");
+        businessLogic.createTour(createTour);
+
+        businessLogic.searchTour("aaaaaaaaaa");
+        */
+
+        /*
+        businessLogic.getTourList();
+
+        businessLogic.getTour(1);
+
+        Tour updateTour = new Tour(); updateTour.setTourDescription("New description"); updateTour.setDestination("Wien Mitte");
+        businessLogic.updateTour(1, updateTour);
+
+
+
+        businessLogic.deleteTour(1);
+
+        businessLogic.getTour(1);
+
+        businessLogic.createTour(createTour);
+
+        businessLogic.getTourLogs(2);
+        businessLogic.getTourLogs(3);
+
+        TourLog createTourLog = new TourLog("My comment", 3, 8500, 2);
+        businessLogic.createTourLog(2, createTourLog);
+        businessLogic.createTourLog(2, createTourLog);
+
+        businessLogic.getTourLogs(2);
+
+        businessLogic.getTourLog(1);
+        businessLogic.getTourLog(2);
+        businessLogic.getTourLog(10);
+
+        TourLog updateTourLog = new TourLog(); updateTourLog.setComment("New comment");
+        businessLogic.updateTourLog(1, updateTourLog);
+
+        businessLogic.getTourLogs(2);
+
+        businessLogic.deleteTourLog(1);
+
+        businessLogic.getTourLog(1);
+
+        businessLogic.getTourLogs(2);
+
+
+        businessLogic.exportTours("C:\\Users\\Felix\\Desktop\\export.json");
+
+        businessLogic.importTours("C:\\Users\\Felix\\Desktop\\import.json");
+        */
+    }
+}
---
+++ Source/Client/src/main/java/at/fhtw/client/services/DataTransferService.java
@@ -0,0 +1,79 @@
+package at.fhtw.client.services;
+
+import at.fhtw.client.exceptions.BadRequestException;
+import at.fhtw.client.exceptions.FailedToSendRequestException;
+import at.fhtw.client.exceptions.NoContentException;
+import at.fhtw.client.exceptions.NotFoundException;
+import at.fhtw.client.models.Tour;
+import at.fhtw.client.utils.JsonFileHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+
+public class DataTransferService {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final HttpClient httpClient = HttpClient.newHttpClient();
+    public List<Tour> exportTours(String filename) {
+        try {
+            HttpRequest httpRequest = HttpRequest.newBuilder()
+                    .uri(new URI("http://127.0.0.1:10001/export"))
+                    .GET()
+                    .build();
+
+            HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+
+            if(httpResponse.statusCode() == 204) {
+                throw new NoContentException("exportTours - no content");
+            }
+            if(httpResponse.statusCode() == 400) {
+                throw new BadRequestException("exportTours - bad request");
+            }
+            if(httpResponse.statusCode() == 404) {
+                throw new NotFoundException("exportTours - not found");
+            }
+
+            JsonFileHandler.saveJsonStringToFile(filename, httpResponse.body());
+
+            List<Tour> tours = List.of(objectMapper.readValue(httpResponse.body(), Tour[].class));
+            return tours;
+        } catch (URISyntaxException | IOException | InterruptedException e) {
+            // e.printStackTrace();
+            throw new FailedToSendRequestException("exportTours - failed to send request");
+        }
+    }
+    public List<Tour> importTours(String filename) {
+        try {
+            String jsonString = JsonFileHandler.readJsonStringFromFile(filename);
+
+            HttpRequest httpRequest = HttpRequest.newBuilder()
+                    .uri(new URI("http://127.0.0.1:10001/import"))
+                    .POST(HttpRequest.BodyPublishers.ofString(jsonString))
+                    .setHeader("Content-Type", "application/json")
+                    .build();
+
+            HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+
+            if(httpResponse.statusCode() == 204) {
+                throw new NoContentException("importTours - no content");
+            }
+            if(httpResponse.statusCode() == 400) {
+                throw new BadRequestException("importTours - bad request");
+            }
+            if(httpResponse.statusCode() == 404) {
+                throw new NotFoundException("importTours - not found");
+            }
+
+            List<Tour> importTours = List.of(objectMapper.readValue(httpResponse.body(), Tour[].class));
+            return importTours;
+        } catch (URISyntaxException | IOException | InterruptedException e) {
+            // e.printStackTrace();
+            throw new FailedToSendRequestException("importTours - failed to send request");
+        }
+    }
+}
